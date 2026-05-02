4dce2d7efea9b360bda78386147cf14dae7d6163
Muhammed Karim <muhammedkarim@hotmail.co.uk>
2026-05-02 17:52:22 +0100

    photos poster on taraweeh system

--- script.js
+++ script.js
@@ -502,7 +502,7 @@ function initPrayerTimes() {
 
   async function pollTaraweehStateAndApply() {
     const now = new Date();
-    if (now.getHours() < 19 || now.getHours() > 22) return
+    // if (now.getHours() < 19 || now.getHours() > 22) return
     try {
       const res = await fetch(`https://taraweeh.muhammedkarim.workers.dev/state/bradford?ts=${Date.now()}`, { cache: "no-store" });
       if (!res.ok) throw new Error(`HTTP ${res.status}`);
@@ -547,7 +547,7 @@ function initPrayerTimes() {
   fetchAnnouncement();
   preloadAndCheckPosters();
   // checkLiveStatusAndToggleOverlay();
-  // pollTaraweehStateAndApply();
+  pollTaraweehStateAndApply();
   
   setInterval(updateClock, 1000);
   setInterval(loadPrayerTimes, 1000);
@@ -559,5 +559,5 @@ function initPrayerTimes() {
   setInterval(refreshPosters, 3600000);
   // setInterval(checkLiveStatusAndToggleOverlay, 5000);
   setInterval(checkVersionAndReload, 60000);
-  // setInterval(pollTaraweehStateAndApply, 1000);
+  setInterval(pollTaraweehStateAndApply, 1000);
 }
